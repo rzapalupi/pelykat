c1b80190b3f75cb5a7cdf875157d3fbdb2a5f53f
Reza Palupi <reza@crrsecurity.com>
2018-10-27 22:32:51 +0700

    initial commit

---
+++ app/src/main/java/com/ariskadm57/pelykat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ariskadm57.pelykat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
